f1185effb52bc5b2d2d6bb94f33d5089466e481c
Brian Fitzgerald <BFitzgerald@JACKSONGOV.ORG>
2020-03-17 09:12:32 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a few numbers separated by a hyphen.");
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+            if (input.Length > 0)
+            {
+                foreach (var number in input.Split('-'))
+                    numbers.Add(Convert.ToInt32(number));
+            }
+            else if (string.IsNullOrWhiteSpace(input))
+                return; 
+            var uniques = new List<int>();
+            var duplicatesExist = false; 
+
+            foreach(var number in numbers)
+            {
+                if (uniques.Contains(number))
+                {
+                    duplicatesExist = true;
+                    break;
+                }
+                else
+                    uniques.Add(number);
+            }
+            var answer = duplicatesExist ? "Duplicates" : "No Duplicates";
+            Console.WriteLine(answer);
+        }
+
+    }
+}
